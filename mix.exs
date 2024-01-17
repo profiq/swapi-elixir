4d1017b7f34c081c7dddc84fd892d1b644f8efe1
David Hrdlička <13226155+dhrdlicka@users.noreply.github.com>
2024-01-17 13:32:08 +0100

    Add SEO library

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule SWAPI.MixProject do
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.1"},
+      {:phoenix_seo, "~> 0.1.8"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.2"},
       {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
