8bb061dd6957443105fedfd7573d423328cf9858
David Hrdlička <13226155+dhrdlicka@users.noreply.github.com>
2024-01-19 11:19:33 +0100

    Use long polling for live view

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,12 +18,12 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
+import {Socket, LongPoll} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, transport: LongPoll})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
--- lib/swapi_web/endpoint.ex
+++ lib/swapi_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule SWAPIWeb.Endpoint do
     same_site: "Lax"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]], longpoll: true
 
   # Serve at "/" the static files from "priv/static" directory.
   #
