a1f592267ba7ffb58b9e56b369c402b9da4045d6
David Hrdlička <13226155+dhrdlicka@users.noreply.github.com>
2024-02-05 16:18:50 +0100

    Add `offset` query parameter as an experiment

--- lib/swapi/util.ex
+++ lib/swapi/util.ex
@@ -9,13 +9,15 @@ defmodule SWAPI.Util do
           previous: non_neg_integer | nil
         }
 
+  @page_size 10
+
   @doc """
   Queries the given Ecto queryable with pagination.
   """
   @spec paginate(Ecto.Queryable.t(), map()) :: {:ok, {list, page_info}} | {:error, atom}
   def paginate(query, params) do
     with {:ok, page} <- get_page_number(params),
-         {list, meta} <- Flop.validate_and_run!(query, %Flop{page: page}) do
+         {list, meta} <- Flop.validate_and_run!(query, %Flop{page: page, page_size: @page_size}) do
       {:ok,
        {list, %{count: meta.total_count, next: meta.next_page, previous: meta.previous_page}}}
     end
@@ -25,6 +27,13 @@ defmodule SWAPI.Util do
     case Integer.parse(page) do
       :error -> {:error, :bad_request}
       {page, _} -> {:ok, page}
+    end
+  end
+
+  defp get_page_number(%{"offset" => offset}) when is_binary(offset) do
+    case Integer.parse(offset) do
+      :error -> {:error, :bad_request}
+      {offset, _} -> {:ok, div(offset, @page_size)}
     end
   end
 
