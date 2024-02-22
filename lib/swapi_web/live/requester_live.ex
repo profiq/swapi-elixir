0eddcbb936c0b02c98fba3243616f3a55017e805
David Hrdlička <13226155+dhrdlicka@users.noreply.github.com>
2024-02-22 16:39:44 +0100

    Add copy button to request box

--- lib/swapi_web/live/requester_live.ex
+++ lib/swapi_web/live/requester_live.ex
@@ -8,6 +8,27 @@ defmodule SWAPIWeb.RequesterLive do
       <div class="input-group">
         <span class="input-group-text"><%= url(~p"/api/") %></span>
         <.input type="text" class="form-control" field={@form[:url]} placeholder={@placeholder} />
+        <button
+          class="btn btn-link border d-flex align-items-center"
+          title="Copy URL"
+          onclick="return false;"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-copy"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
+            <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
+          </svg>
+        </button>
         <button class="btn btn-primary d-flex align-items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
