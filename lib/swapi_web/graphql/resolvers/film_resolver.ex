84608863b098b89104d3436333f64590c46e2053
David Hrdlička <13226155+dhrdlicka@users.noreply.github.com>
2024-03-27 14:30:29 +0100

    Add GraphQL schema, Film type and queries

---
+++ lib/swapi_web/graphql/resolvers/film_resolver.ex
@@ -0,0 +1,20 @@
+defmodule SWAPIWeb.GraphQL.Resolvers.FilmResolver do
+  @moduledoc """
+  Film resolver.
+  """
+
+  alias SWAPI.Films
+
+  @spec all(map, map) :: {:ok, list(Film.t())} | {:error, any}
+  def all(_args, _info) do
+    {:ok, Films.list_films()}
+  end
+
+  @spec one(map, Absinthe.Blueprint.t()) :: {:ok, Film.t()} | {:error, any}
+  def one(%{id: id}, _info) do
+    case Films.get_film(id) do
+      {:ok, film} -> {:ok, film}
+      {:error, :not_found} -> {:error, "Film not found"}
+    end
+  end
+end
