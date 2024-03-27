48ec603140bdad22990680be7c83c4b9e4620df3
David Hrdlička <13226155+dhrdlicka@users.noreply.github.com>
2024-03-27 14:24:02 +0100

    Add Absinthe dependency

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,11 @@
 [
   import_deps: [:ecto, :ecto_sql, :phoenix],
   subdirectories: ["priv/*/migrations"],
-  plugins: [Phoenix.LiveView.HTMLFormatter],
-  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}", "priv/*/seeds.exs"]
+  plugins: [Phoenix.LiveView.HTMLFormatter, Absinthe.Formatter],
+  inputs: [
+    "*.{ex,exs}",
+    "{config,lib,test}/**/*.{ex,exs}",
+    "priv/*/seeds.exs",
+    "{lib,priv}/**/*.{gql,graphql}"
+  ]
 ]
