8731b99bb7007d2dc33db8cb75ec9d04000b1bc8
David Hrdlička <13226155+dhrdlicka@users.noreply.github.com>
2025-03-21 11:37:20 +0100

    Fix alias warnings

--- lib/swapi_web/graphql/queries/starship_queries.ex
+++ lib/swapi_web/graphql/queries/starship_queries.ex
@@ -1,10 +1,12 @@
 defmodule SWAPIWeb.GraphQL.Queries.StarshipQueries do
   use Absinthe.Schema.Notation
 
+  alias SWAPIWeb.GraphQL.Resolvers.StarshipResolver
+
   object :starship_queries do
     @desc "Get all starships."
     field :all_starships, list_of(:starship) do
-      resolve(&SWAPIWeb.GraphQL.Resolvers.StarshipResolver.all/2)
+      resolve(&StarshipResolver.all/2)
     end
 
     @desc "Get a starship by ID."
@@ -12,7 +14,7 @@ defmodule SWAPIWeb.GraphQL.Queries.StarshipQueries do
       @desc "The ID of the starship."
       arg(:id, non_null(:id))
 
-      resolve(&SWAPIWeb.GraphQL.Resolvers.StarshipResolver.one/2)
+      resolve(&StarshipResolver.one/2)
     end
 
     @desc "Search starships by name or model."
@@ -20,7 +22,7 @@ defmodule SWAPIWeb.GraphQL.Queries.StarshipQueries do
       @desc "A list of search terms. If multiple search terms are used then objects will be returned in the list only if all the provided terms are matched."
       arg(:search_terms, non_null(list_of(non_null(:string))))
 
-      resolve(&SWAPIWeb.GraphQL.Resolvers.StarshipResolver.search/2)
+      resolve(&StarshipResolver.search/2)
     end
   end
 end
